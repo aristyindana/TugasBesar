9374519f6e365eba15e6bd977c1f5a452c436bca
aristyindana <zulfa.94arinda@gmail.com>
2015-05-21 08:43:17 +0700

    Update IdentitasRK.java

--- TugasBesar/src/TugasBesar/IdentitasRK.java
+++ TugasBesar/src/TugasBesar/IdentitasRK.java
@@ -1,34 +1,34 @@
 package TugasBesar;
 
-abstract public class IdentitasRK {
+import java.util.Scanner;
+
+public class Input_LingkunganRK extends Input_IdentitasRK {
+Test_LingkunganRK lingkup = new Test_LingkunganRK();
     
-    private String namaRK;
-    private String lokasiRK;
-    private String programstudi_fakultas;
+    void analisis() {
+    Scanner x = new Scanner(System.in);
+    System.out.println("Analisis kondisi lantai :");
+    String a = (x.next());
+    System.out.println("Analisis kondisi dinding :");
+    String b = (x.next());
+    System.out.println("Analisis kondisi atap :");
+    String c = (x.next());
+    System.out.println("Analisis kondisi pintu :");
+    String d = (x.next());
+    System.out.println("Analisis kondisi jendela :");
+    String e = (x.next());
+    
+    }
+    @Override
+void analisis_identitasRK(){
+    Scanner x = new Scanner(System.in);
+        System.out.print("Masukkan Nama Ruang : ");
+        String nm = (x.next());
+        System.out.print("Masukkan Lokasi Ruang : ");
+        String lk = (x.next());
+        System.out.print("Masukkan Program Studi/Fakultas : ");
+        String pf = (x.next());
+    
+}
 
-    public String getNamaRK() {
-        return namaRK;
-    }
-
-    public void setNamaRK(String namaRK) {
-        this.namaRK = namaRK;
-    }
-
-    public String getLokasiRK() {
-        return lokasiRK;
-    }
-
-    public void setLokasiRK(String lokasiRK) {
-        this.lokasiRK = lokasiRK;
-    }
-
-    public String getProgramstudi_fakultas() {
-        return programstudi_fakultas;
-    }
-
-    public void setProgramstudi_fakultas(String programstudi_fakultas) {
-        this.programstudi_fakultas = programstudi_fakultas;
-    }
-            
-    abstract void analisis ();
-    }
+}
